9e36ad91fd88357c02cbfb2e81c080ffd3c05fd0
Gabrielli George <gabrielli.george@gmail.com>
2017-03-19 12:19:23 +0200

    added Insurance repository, needs some thinkering on the date

--- src/repository/InsuranceFileRepository.java
+++ src/repository/InsuranceFileRepository.java
@@ -1,13 +1,74 @@
 package repository;
 
+import domain.Insurance;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /**
  * Created by gobi on 3/19/17.
  */
 public class InsuranceFileRepository implements IRepository
 {
+    Map<Integer, Insurance> insurances;
+
+    public InsuranceFileRepository()
+    {
+        insurances = new HashMap<>();
+        readFile();
+    }
+
     @Override
     public void readFile()
     {
+        Path fileName = Paths.get("insurances.txt");
 
+        try
+        {
+            Files.lines(fileName).forEach(
+                    line ->
+                    {
+                       try
+                       {
+                           List<String> attributes = Arrays.asList(line.split(","));
+
+
+                        int ID = Integer.parseInt(attributes.get(0));
+                        int carID = Integer.parseInt(attributes.get(1));
+                        String issuer = attributes.get(2);
+                        SimpleDateFormat parser = new SimpleDateFormat("dd.MM.YYYY");
+                        Date expiration = parser.parse(attributes.get(3));
+                        Date start = parser.parse(attributes.get(4));
+
+                           Insurance temp = new Insurance(ID, carID, issuer, expiration, start);
+                           save(temp);
+                       }
+                       catch(ParseException e)
+                       {
+                           e.printStackTrace();
+                       }
+                    }
+            );
+        }
+
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+
+    }
+
+    public Optional<Insurance> save(Insurance newInsurance)
+    {
+        if(newInsurance == null)
+        {
+            throw new IllegalArgumentException("Insurance is not good!");
+        }
+        else return Optional.ofNullable(insurances.putIfAbsent(newInsurance.getID(), newInsurance));
     }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 import repository.CarFileRepository;
 import repository.IRepository;
+import repository.InsuranceFileRepository;
 
 /**
  * Created by gobi on 3/19/17.
@@ -11,5 +12,6 @@ public class Main
     public static void main(String[] args)
     {
         IRepository carRepository = new CarFileRepository();
+        IRepository insuranceRepository = new InsuranceFileRepository();
     }
 }
